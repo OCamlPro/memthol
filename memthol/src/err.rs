6dd65d2569da5192c66f4d3ed7353d99ade5dd19
Adrien Champion <adrien.champion@email.com>
2020-08-06 10:10:29 +0200

    (more) graceful handling of client hangups during pings

--- memthol/src/err.rs
+++ memthol/src/err.rs
@@ -1,22 +1,4 @@
 //! Memthol's server errors.
 
 pub use charts::err::*;
-
-#[macro_export]
-macro_rules! unwrap {
-    ($e:expr) => {
-        match $e {
-            Ok(res) => res,
-            Err(e) => {
-                println!("|===| Error:");
-                for e in e.iter() {
-                    for line in format!("{}", e).lines() {
-                        println!("| {}", line)
-                    }
-                }
-                println!("|===|");
-                std::process::exit(0)
-            }
-        }
-    };
-}
+pub use charts::unwrap_or;
